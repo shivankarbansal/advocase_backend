7fd77c99b3226dbea1130f80cb463de0032e8201
shivankarbansal <74249113+shivankarbansal@users.noreply.github.com>
2022-07-03 18:49:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const caseRouter = require("./routes/caseRoutes");
 const lawyerRouter = require("../advocase_backend/routes/lawyerRoutes");
 const newsRouter = require("./routes/newsRoutes");
 const app = express();
+import eventRoutes from './routes/eventRoutes.js';
 
 // MIDDLEWARES
 if (process.env.NODE_ENV == "development") {
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use("/api/v1/cases", caseRouter);
 app.use(lawyerRouter);
 app.use("/api/v1/news", newsRouter);
+app.use('/api/v1/events', eventRoutes);
 
 module.exports = app;
